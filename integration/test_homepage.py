c1727a0054aa5a198704ca1582c8900edbef9e4a
Krishna Upadhyay <krishnaupadhyay1997@gmail.com>
2021-04-28 23:56:03 +0545

    Add a random assertion

--- integration/test_homepage.py
+++ integration/test_homepage.py
@@ -1,6 +1,8 @@
 class TestHomepage:
     def test_one(self, driver):
         driver.get("https://www.google.com")
+        assert False
 
     def test_two(self, driver):
         driver.get("https://www.facebook.com")
+        assert True
